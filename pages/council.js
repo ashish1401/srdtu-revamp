6f135c9c653ba412f6288be33b1fb15318e25eed
kartik1601 <kartiku904@gmail.com>
2023-08-27 16:16:36 +0530

    Cards hover update
    Cards animation updated with instagram, linkedIn and github urls.

--- pages/council.js
+++ pages/council.js
@@ -4,18 +4,39 @@ import Link from 'next/link';
 import React, {useState} from 'react';
 import { SeniorcouncilMembers, JuniorcouncilMembers } from '../constants';
 import { LinkedinShareButton, LinkedinIcon,} from 'next-share';
-import {FaInstagram, FaInstagramSquare} from 'react-icons/fa'
+import {FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa'
 
 
 function InstagramButton({ url }) {
-    const instagramUrl = `${encodeURIComponent(url)}`;
+    const instagramUrl = url;
   
     return (
       <Link href={instagramUrl} target="_blank" rel="noopener noreferrer">
-        <FaInstagram size={32} style={{ color: '#ff69b4', borderRadius: '0%', background: 'transparent' }}/>
+        <FaInstagram size={26} style={{ color: '#fff', border: 'none', background: 'transparent' }}/>
+      </Link>
+    );
+}
+
+function LinkedInButton({ url }) {
+    const linkedInUrl = url;
+  
+    return (
+      <Link href={linkedInUrl} target="_blank" rel="noopener noreferrer">
+        <FaLinkedin size={26} style={{ color: '#fff', border: 'none', background: 'transparent' }}/>
       </Link>
     );
   }
+
+  function GitHubButton({ url }) {
+    const githubUrl = url;
+  
+    return (
+      <Link href={githubUrl} target="_blank" rel="noopener noreferrer">
+        <FaGithub size={26} style={{ color: '#fff', border: 'none', background: 'transparent' }}/>
+      </Link>
+    );
+  }
+  
 
 
 const Council = () => {
@@ -30,7 +51,7 @@ const Council = () => {
                 <title>Council - SR DTU</title>
             </Head>
             {/* Buttons for navigation */}
-            <div className="min-h-screen bg-gradient-to-l from-blue-100 via-blue-200 to-blue-200 flex flex-col justify-center items-center">
+            <div className="min-h-screen bg-white flex flex-col justify-center items-center">
                 <h1 className={`text-[52px] mt-[80px] mb-[20px] font-black bg-clip-text text-transparent bg-gradient-to-t from-indigo-900 via-slate-800 to-indigo-900 lg:text-[72px] sm:text-[60px] xs:text-[50px] lg:leading-[98px]`}>COUNCIL 2022-2023</h1>
                 <div className="flex gap-12 items-center">
                     <button
@@ -48,36 +69,40 @@ const Council = () => {
                 </div>
                 {/* Cards Content for members */}
                 <div className="mt-10 mb-20">
-                    {activeButton === 'senior' && (
-                        <div className="">
-                            <div className={styles.container}>
-                                {/* cards - senior council */}
-                                <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 pl-[10px] pr-[10px] justify-between`}>
-                                    {SeniorcouncilMembers.map((member, index) => (   
-                                        <div class="relative w-[300px] rounded-xl shadow bg-gray-800 border-gray-700">
-                                            {/* Profile image */}
-                                            <img src={`images/Council/SeniorCouncilMember${index + 1}.jpg`} alt="Profile" className={`w-[300px] h-[300px] rounded-t-xl`} />
-                                            {/* Info */}
+                {activeButton === 'senior' && (
+                    <div className="">
+                        <div className={styles.container}>
+                            {/* Cards - Senior Council */}
+                            <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 pl-[10px] pr-[10px] justify-between`}>
+                                {SeniorcouncilMembers.map((member, index) => (
+                                    <div className={`${styles.cardContainer} bg-stone-800 outline outline-indigo-700 `}>
+                                        <div className='relative'>
+                                            <img
+                                                src={`images/Council/SeniorCouncilMember${index + 1}.jpg`}
+                                                alt="Profile"
+                                                className={`w-[300px] h-[280px] rounded-t-xl`}
+                                            />
                                             <div class="p-5">
                                                 <h5 class="mb-2 text-3xl font-bold tracking-tight text-amber-500">{member.name}</h5>
                                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-slate-300">{member.position}</h5>
-                                                <p class="mb-3 font-normal  text-gray-400">{member.info}</p>
-                                                
-                                                <div className='flex flex-row gap-4  pt-3'>
-                                                    <InstagramButton url={member.insta}/>
-                                                    <LinkedinShareButton
-                                                        url={member.linkedin} >
-                                                        <LinkedinIcon size={32} round />
-                                                    </LinkedinShareButton>
-                                                </div>
-                                                
+                                            </div> 
+                                        </div>
+                                        <div className={`${styles.cardContent} h-full backdrop-filter backdrop-blur-2xl`}>
+                                            <p className='text-[17px] text-white font-semibold mb-10'>{member.info}</p>
+                                            <div className='flex flex-row gap-6 justify-start items-start'>
+                                                <InstagramButton url={member.insta} />
+                                                <LinkedInButton url={member.linkedin} />
+                                                <GitHubButton url={member.github} />
                                             </div>
                                         </div>
-                                    ))}
-                                </div>
+                                    </div>
+                                ))}
                             </div>
                         </div>
+                    </div>
                     )}
+
+
 
                     {activeButton === 'junior' && (
                         <div className="">
@@ -85,20 +110,24 @@ const Council = () => {
                             {/* cards - junior council */}
                             <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 pl-[20px] pr-[10px] justify-between`}>
                                 {JuniorcouncilMembers.map((member, index) => (   
-                                    <div class="relative w-[280px] rounded-xl shadow bg-gray-800 border-gray-700">
-                                        {/* Profile image */}
-                                        <img src={`images/Council/JuniorCouncilMember${index + 1}.jpg`} alt="Profile" className={`w-[300px] h-[300px] rounded-t-xl`} />
-                                        {/* Info */}
-                                        <div class="p-5">
-                                            <h5 class="mb-2 text-3xl font-bold tracking-tight text-amber-500">{member.name}</h5>
-                                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-slate-300">{member.position}</h5>
-                                            
-                                            <div className='flex flex-row gap-4  pt-3'>
-                                                    <InstagramButton url={member.insta}/>
-                                                    <LinkedinShareButton
-                                                        url={member.linkedin} >
-                                                        <LinkedinIcon size={32} round />
-                                                    </LinkedinShareButton>
+                                    <div className={`${styles.cardContainer} bg-stone-800 outline outline-indigo-700 `}>
+                                        <div className=''>
+                                            <img
+                                                src={`images/Council/JuniorCouncilMember${index + 1}.jpg`}
+                                                alt="Profile"
+                                                className={`w-[300px] h-[280px] rounded-t-xl`}
+                                            />
+                                            <div class="p-5">
+                                                <h5 class="mb-2 text-3xl font-bold tracking-tight text-amber-500">{member.name}</h5>
+                                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-slate-300">{member.position}</h5>
+                                            </div> 
+                                        </div>
+                                        <div className={`${styles.cardContent} h-[120px] backdrop-filter backdrop-blur-xl`}>
+                                            <p className='text-white font-semibold mb-5'>{member.info}</p>
+                                            <div className='flex flex-row gap-6 justify-start items-start'>
+                                                <InstagramButton url={member.insta} />
+                                                <LinkedInButton url={member.linkedin} />
+                                                <GitHubButton url={member.github} />
                                             </div>
                                         </div>
                                     </div>
