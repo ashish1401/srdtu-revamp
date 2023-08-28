d01fc79727a90c2b1e742e72953c94afefbaa31d
kartik1601 <kartiku904@gmail.com>
2023-08-28 23:20:11 +0530

    fixed merge errors

--- pages/index.js
+++ pages/index.js
@@ -35,12 +35,20 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='bg-black pt-4'>
+      <section className=' pt-4' style={{ backgroundImage: "url('bg/9.png')", backgroundSize: '100% 100%' }}>
         <Heading heading="Past Events" />
-        <div className='lg:grid-cols-2 lg:grid bg-no-repeat bg-cover bg-left-bottom md:bg-contain md:bg-right-bottom' style={{ backgroundSize: "" }}>
+        <div className='lg:grid-cols-2 lg:grid bg-[url(/images/robot.svg)] bg-no-repeat bg-cover bg-left-bottom md:bg-contain md:bg-right-bottom' style={{ backgroundSize: "" }}>
           <div className='flex justify-center items-center h-[700px] md:h-[800px] opacity-90 '>
             <EventsCarousel className="" />
           </div>
+          <div>
+
+          </div>
+
+
+          {/* <div className='h-[400px] mb-10 bg-secondary rounded-xl w-[75%] mx-auto'> */}
+
+          {/* </div> */}
         </div>
       </section >
 
